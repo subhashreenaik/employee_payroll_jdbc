7028f8c8ad09dd8a4cd0f49104aebe6983e256c2
Subhashree naik <subhashree.naik@gmail.com>
2022-03-31 13:22:42 +0530

    Welcome message

---
+++ main/java/com/EmployeePayrollJDBC/App.java
@@ -0,0 +1,10 @@
+package com.EmployeePayrollJDBC;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Employee_payroll JDBC program " );
+    }
+}
